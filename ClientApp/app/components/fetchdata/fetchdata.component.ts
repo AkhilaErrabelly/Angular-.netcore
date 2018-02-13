8489f1d5749933ddef95a370c99a1022843ebce8
akhila errabelly <akhila.errabelly@oakland.k12.mi.us>
2018-02-13 13:23:19 -0500

    asdasdad

---
+++ ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'fetchdata',
+    templateUrl: './fetchdata.component.html'
+})
+export class FetchDataComponent {
+    public forecasts: WeatherForecast[];
+    public cacheForecasts: WeatherForecast[];
+    public summeries: any[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+            this.forecasts = result.json() as WeatherForecast[];
+        }, error => console.error(error));
+        http.get(baseUrl + 'api/SampleData/WEatherForecasts').subscribe(result => {
+            this.summeries = result.json() as any[];
+        }, error => console.error(error));
+    }
+}
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
